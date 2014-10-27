c9eaab7ae815c58fc164032c4f11a56f82d0009b
jethro894 <jethro@live.cn>
2014-10-28 07:49:12 +0800

    count and say updated

--- src/com/leet/CountAndSay.java
+++ src/com/leet/CountAndSay.java
@@ -10,44 +10,70 @@ public class CountAndSay {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		countAndSay(3);
+		List<Pair> list = strToCount("sdzdffggfggfghghj3");
 	}
 	
 	public static void countAndSay(int n) {
-        /*if(n < 1)
-        	return "0";
-        if(n == 1)
-        	return "1";*/
+        if(n <= 0)
+        	return;
         
-        char[] preArray = "1".toCharArray();
-        List<Character> charset;
-        List<Integer> numbers;
+        String pre;
+        String current = "1";
+        List<Pair> list;
         for(int i = 1; i <= n; i++){
-        	charset = new ArrayList<Character>();
-        	numbers = new ArrayList<Integer>();
+        	pre = current;
+        	list = 
         	
-        	char lastChar = "~".toCharArray()[0];
-        	int lastCharFirstAppearIndex = 0;
         	
-        	for(int j = 0; j < preArray.length; j++){
-        		if(preArray[j] != lastChar){
-        			charset.add(preArray[j]);
-        			numbers.add(j-lastCharFirstAppearIndex + 1);
-        			lastChar = preArray[j];
-        			lastCharFirstAppearIndex = j;
-        		}
-        		else{
-        			String s = "";
-        		}
-        	}
-        	
-        	System.out.println("n = " + i);
-        	for(char c : charset)
-        		System.out.print(c + ",");
-        	System.out.println();
-        	for(int k : numbers)
-        		System.out.print(k + ",");
-        	System.out.println();
+        	System.out.print(current + ", ");
         }
+        
+        
     }
+	
+	public static List<Pair> strToCount(String str){
+		char[] array = str.toCharArray();
+		List<Pair> list = new ArrayList<Pair>();
+		
+		if(str.length() == 0)
+			return list;
+		
+		char pre = array[0];
+		int count = 0;
+		for(char c : array){
+			if (c != pre){
+				list.add(new Pair(pre,count));
+				pre = c;
+				count = 1;
+			}
+			else{
+				count++;
+			}
+		}
+		list.add(new Pair(array[array.length-1], count));
+		return list;
+	}
+	
+	public static String countToStr(List<Pair> list){
+		if(list.size() == 0)
+			return (String)null;
+		
+		StringBuilder sb = new StringBuilder();
+		for(Pair p : list){
+			for(int i = p.value; i > 0; i--){
+				sb.append(p.key);
+			}
+		}
+		
+		return sb.toString();
+	}
+}
+
+class Pair{
+	char key;
+	int value;
+	public Pair(char c, int i){
+		this.key = c;
+		this.value = i;
+	}
 }
