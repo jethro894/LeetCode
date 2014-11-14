350678e6ca8a167865ebd4a6602e3c203fb9df77
jethro894 <jethro@live.cn>
2014-11-14 11:48:31 +0800

    2014-11-13-22:48

--- src/Subset.java
+++ src/Subset.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 public class Subset {
@@ -10,7 +12,9 @@ public class Subset {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] s = {1,2,3};
+		for(List<Integer> list : new Subset().subsets(s))
+			System.out.println(list);
 	}
 
 	List<List<Integer>> result;
@@ -20,6 +24,24 @@ public class Subset {
         	return result;
         Arrays.sort(S);
         
+        List<Integer> empty = new ArrayList<Integer>();
+        grow(empty, 0, S);
         
+        Set<List<Integer>> distincts = new HashSet<List<Integer>>(result);
+        return new ArrayList<List<Integer>>(distincts);
     }
+	
+	public void grow(List<Integer> pre, int index, int[] s){
+		if(index == s.length){
+			result.add(pre);
+			return;
+		}
+		
+		List<Integer> withThisDigit = new ArrayList<Integer>(pre);
+		withThisDigit.add(s[index]);
+		grow(withThisDigit, index + 1, s);
+		
+		List<Integer> withoutThisDigit = new ArrayList<Integer>(pre);
+		grow(withoutThisDigit, index + 1, s);
+	}
 }
